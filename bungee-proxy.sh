7ca947211442bf3feea85f6ab6ef5064e6c4f1c1
silverace_71 <silver@silverhub.xyz>
2023-02-21 13:41:19 -0700

    added start script

--- bungee-proxy.sh
+++ bungee-proxy.sh
@@ -6,3 +6,10 @@ bv=$(echo "$latest_build_version" | sed -e 's/^"//' -e 's/"$//')
 latest_build1="$(curl -sX GET https://api.papermc.io/v2/projects/waterfall/versions/"$bv"/builds -H 'accept: application/json' | jq '.builds [-1].build')"
 download_url1=""$api"/projects/"$name1"/versions/"$bv"/builds/"$latest_build1"/downloads/"$name1"-"$bv"-"$latest_build1".jar"
 wget $download_url1
+mv "$name1"-"$bv"-"$latest_build1".jar waterfall.jar
+cat << EOF >> start.sh
+#!/bin/bash
+java -Xms512M -Xmx512M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -jar waterfall.jar
+EOF
+sudo chmod +x start.sh
+echo "Use ./start.sh to start the Proxy Server!"
