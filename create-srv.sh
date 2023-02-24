e5c65bf001743054813516fa12efa936b7166bac
silverace_71 <silver@silverhub.xyz>
2023-02-24 16:05:51 -0700

    paper and bungee supported

--- create-srv.sh
+++ create-srv.sh
@@ -2,44 +2,48 @@
 echo "do you wish to run SilverServer setup (y/n)?"
 read r0
 if [[ $r0 =~ ^[Yy]$ ]]; then
-    echo -e "Have you installed java yet (y/n)?"
-    read r0
-    if [[ $r0 =~ ^[Yy]$ ]]; then
+    if echo "$git -v" | grep -q "^$version"; then
+    sleep 0
+    else
+    sudo apt install git -y
+    fi
+    if echo "$java -version" | grep -q "^$version"; then
     sleep 0
     else
     sudo apt install openjdk-17-jdk -y
     fi
+    if echo "$jq --help" | grep -q "^$manpage"; then
+    sleep 0
+    else
+    sudo apt install jq -y
+    fi
     echo -e "What server will this be?"
     PS3="Choose an option:"
-    select option in "1||Paper" "2||Fabric" "3||Forge" "4||Vanilla"; do
+    select option in "1||Paper" "2||Fabric" "3||Forge" "4||Vanilla" "5||Bungee-Proxy"; do
     case $option in
         "1||Paper")
         wget https://raw.githubusercontent.com/silverace71/QuickMCServer/main/paper-server.sh
         sudo chmod +x paper-server.sh
-        exit
+        ./paper-server.sh
         break
         ;;
         "2||Fabric")
         echo "You selected Option 2"
-        exit
         break
         ;;
         "3||Forge")
         echo "You selected Option 3"
-        exit
         break
         ;;
         "4||Vanilla")
         echo "You selected Option 3"
-        exit
         break
         ;;
-        "5||Bungeecord Proxy")
+        "5||Bungee-Proxy")
         wget https://raw.githubusercontent.com/silverace71/QuickMCServer/main/bungee-proxy.sh
         sudo chmod +x bungee-proxy.sh
         ./bungee-proxy.sh
-        sleep 0
-        exit
+        break
         ;;
         *)
         echo "Invalid selection"
@@ -51,5 +55,3 @@ else
 echo "install cancelled"
 exit
 fi
-echo "done!"
-exit
